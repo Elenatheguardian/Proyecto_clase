3f776c28f5620f53774d137cc32ff906bd506608
Elena <elena.alexandru.1999@gmail.com>
2024-05-10 09:51:54 +0200

    junto todo en la misma carpeta

---
+++ src/main/java/modelo/Usuario.java
@@ -0,0 +1,127 @@
+package modelo;
+
+import java.sql.SQLException;
+import com.google.gson.*;
+import dao.DaoUsuario;
+public class Usuario {
+	
+	private int id;
+	private String nombre;
+	private String mail;
+	private String tel;
+	private int permiso;
+	
+	
+
+	/**
+	 *  Es el paso del usuario para que pueda tener cuenta en la pagina 
+	 */
+	public Usuario() {
+		
+		
+	}
+	 
+	public Usuario( String nombre, String mail, String tel, int permiso) throws SQLException {
+	this.nombre = nombre;
+	this.mail = mail;
+	this.tel = tel;
+	this.permiso = permiso;
+	}
+	
+	public Usuario(int id, String nombre, String mail, String tel, int permiso) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.mail = mail;
+		this.tel = tel;
+		this.permiso = permiso;
+	}
+
+	public int getid() {
+		return id;
+	}
+		
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre =nombre;
+	
+	}
+	public String getMail() {
+		return mail;
+	}
+	public void setTel(String tel) {
+		this.tel=tel;
+	}
+	public int getPermiso(int permiso) {
+		return permiso;
+		
+	}
+	public void setPermiso(int permiso) {
+		this.permiso =permiso;
+	}
+	
+	/*public void obtenerPorId(int id) throws SQLException {
+		
+		DaoUsuario dao = new DaoUsuario();
+		Usuario aux= dao.obtenerPorID(id);
+		
+		this.setId(aux getId());
+		this.setNombre(aux.getNombre());
+		this.setTel(aux.getMail());
+		this.setPermiso(aux.getPermiso());
+	}
+	
+	public boolean logeo(String pass) throws SQLException{
+		boolean ok = false;
+		DaoUsuario dao = new DaoUsuario();
+		Usuario aux= dao.logeando(this, pass); // db
+		
+		if(aux != null) {
+			ok=true;
+			this.setId(aux.getId());
+			this.setNombre(aux.getNombre());
+			this.setTel(aux.getTel());
+			this.setMail(aux.getMail());
+			this.setPermiso(aux.getPermiso());
+		}
+			return ok;
+		}
+	public String dameJson() {
+		String json = "";
+		
+		Gson gson= new Gson();
+		
+		json = gson.toJson(this);
+		
+		return json;
+			
+	}
+	public void borrar(int id) throws SQLException{
+		DaoUsuario dao = new DaoUsuario();
+		dao.borrar(id);
+		
+	}
+	public String toString() {
+		return "Usuario [id=" +id +", nombre="+ nombre +", mail=" +mail+", permiso=" +permiso +"]";
+		
+	}*/
+	
+	
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
