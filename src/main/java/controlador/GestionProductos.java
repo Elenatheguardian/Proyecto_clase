f44fcb356336b2e1a89794b039e60911879f9d48
Elena <elena.alexandru.1999@gmail.com>
2024-05-11 16:40:17 +0200

    2

--- src/main/java/controlador/GestionProductos.java
+++ src/main/java/controlador/GestionProductos.java
@@ -1,20 +1,33 @@
 package controlador;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.MultipartConfig;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.Part;
 import modelo.Producto;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.sql.SQLException;
+
+
+
 
 /**
  * Servlet implementation class GestionProductos
  */
+@MultipartConfig
 public class GestionProductos extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private String pathFiles = "F:\\eclipse java\\proyectos\\Proyecto_clase\\src\\main\\webapp\\fotos_productos";
+	private File upload = new File(pathFiles);
 
     /**
      * Default constructor. 
@@ -36,14 +49,28 @@ public class GestionProductos extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		this.insertar(request, response);
+	}
 		
+		
+		private void insertar(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String tipo = request.getParameter("Tipo");
 		String nombre = request.getParameter("Nombre");
 		String descripcion = request.getParameter("Descripcion");
 		float precio = Float.parseFloat(request.getParameter("Precio"));
 		
-		Producto p1= new Producto(tipo,nombre,descripcion,precio);
-		//System.out.println(p1.toString());
+		Part part = request.getPart("foto");	
+		Path path = Paths.get(part.getSubmittedFileName());
+		String fileName = path.getFileName().toString();
+		InputStream input = part.getInputStream();
+		
+		File file = new File(upload, fileName);
+		
+
+		Files.copy(input,file.toPath());
+		
+		
+		Producto p1= new Producto(tipo, nombre, descripcion, fileName, precio);
 		
 		try {
 			p1.insertar();
@@ -56,5 +83,5 @@ public class GestionProductos extends HttpServlet {
 		
 	}
 	
-	
 }
+
--- src/main/java/dao/DaoProducto.java
+++ src/main/java/dao/DaoProducto.java
@@ -19,12 +19,13 @@ public static Connection con = null;
 	
 	public void Insertar(Producto p) throws SQLException {
 		
-		String sql = "INSERT INTO productos (tipo, nombre,descripcion,precio) VALUES (?,?,?,?)";
+		String sql = "INSERT INTO productos (tipo, nombre,descripcion,precio,foto) VALUES (?,?,?,?,?)";
 		PreparedStatement ps = con.prepareStatement(sql);
 		ps.setString(1, p.getTipo());
 		ps.setString(2, p.getNombre());
 		ps.setString(3, p.getDescripcion());
 		ps.setFloat(4, p.getPrecio());
+		ps.setString(5, p.getFoto());
 		
 		int filas = ps.executeUpdate();
 		ps.close();
